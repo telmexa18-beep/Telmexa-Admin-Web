acdeafeef9e3c790f61f2a25f3998ee8a9e89af2
telmexa18-beep <telmexa18@gmail.com>
2026-01-14 23:53:42 -0600

    Actualizar workers section

--- components/sections/overview-section.tsx
+++ components/sections/overview-section.tsx
@@ -12,21 +12,6 @@ import {
   ResponsiveContainer,
 } from "recharts"
 
-// React Leaflet
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-import L from "leaflet"
-
-// Fix icon Leaflet
-delete (L.Icon.Default.prototype as any)._getIconUrl
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-})
-
 const API_CLIENTS = "https://telmex-backend.onrender.com/api/clients"
 const API_INCIDENCIAS = "https://telmex-backend.onrender.com/api/incidents"
 const API_WORKERS = "https://telmex-backend.onrender.com/api/workers"
